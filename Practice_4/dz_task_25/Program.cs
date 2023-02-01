55c8f9e531b163696ae04394a31c03372d27c2be
Anna <annahuanna666666@gmail.com>
2023-02-01 14:48:15 +0300

    Решено дз 25

---
+++ Practice_4/dz_task_25/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/ 
+
+Console.Clear();
+
+int GetUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+double Action(int num, int deg)
+{
+    double result = Math.Pow(num, deg);
+    return result;
+}
+
+int number = GetUserData("Введите число ");
+int degree = GetUserData("Введите степень ");
+double result = Action(number, degree);
+Console.WriteLine($"Результатом возведения числа {number} в степень {degree} является = {result}");
