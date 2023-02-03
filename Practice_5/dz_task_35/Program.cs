26ed10a3582c539639ce313ca74a553af4a62c77
Anna <annahuanna666666@gmail.com>
2023-02-03 18:20:52 +0300

    решена задача 35

---
+++ Practice_5/dz_task_35/Program.cs
@@ -0,0 +1,47 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+Console.Clear();
+
+int [] GetArrayRandom(int length)
+{
+    int [] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+    return arr;
+}
+
+int Chet_Count(int [] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0 ) count++;
+    }
+    return count;
+}
+
+void showArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i != arr.Length -1)
+        {
+            Console.Write($"{arr[i]}, "); 
+        }
+        else
+        {
+            Console.Write($"{arr[i]}]"); 
+        }
+          
+    }
+}
+
+int [] array = GetArrayRandom(8);
+showArray(array);
+Console.WriteLine();
+int countt = Chet_Count(array);
+Console.WriteLine($"В данном массиве {countt} четных чисел");
