d28c87daa19444bafbc73833635e2c81e6737f1b
Anna <annahuanna666666@gmail.com>
2023-02-16 20:07:14 +0300

    решен задача 56

---
+++ Practice_8/dz__task_56/Program.cs
@@ -0,0 +1,108 @@
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки 
+с наименьшей суммой элементов: 1 строка
+*/
+
+Console.Clear();
+
+int[,] GenerateArray(int colLength, int rowLength, int start, int finish)
+{
+    int [,] array = new int[colLength, rowLength];
+    for (int i = 0; i < colLength; i++)
+    {
+        for (int j = 0; j < rowLength; j++)
+        {
+            array[i,j] = new Random().Next(start, finish +1);
+        }
+    }
+    return array;
+}
+
+void PrintInColor(string data, ConsoleColor color)
+{
+     Console.ForegroundColor = color;
+     Console.Write(data);
+     Console.ResetColor();
+}
+
+void printHeadOfArray(int length)
+{
+    Console.Write(" \t");
+    for (int i = 0; i < length; i++)
+    {
+        PrintInColor(i + "\t", ConsoleColor.DarkGreen);
+    }
+    Console.WriteLine();
+}
+
+
+void PrintArray(int [,] array)
+{
+    printHeadOfArray(array.GetLength(1));
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        PrintInColor(i+"\t", ConsoleColor.Cyan);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [] GetSumm(int[,] array)
+{
+    int [] arr2 = new int [array.GetLength(0)];
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           sum = sum + array[i, j];
+        }
+        arr2[i] = sum;
+    }
+    return arr2;
+}
+
+
+int MinSum(int[] array)
+{
+   int mini = 0; 
+   for (int i = 0; i < array.Length-1; i++) 
+   {
+       mini = i;
+       for (int j = i + 1; i < array.Length; j++)
+       {
+           if(array[j] < array[mini]) mini = j; 
+       }
+   }   
+   return mini;
+}
+
+
+
+void PrintArray1D(int[] array)
+{
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} "); 
+    }
+}
+
+int [,] array = GenerateArray(5, 5, -20, 20);
+PrintArray(array);
+int [] sumArray = GetSumm(array);
+Console.WriteLine("Сумма в каждой строке равна:");
+PrintArray1D(sumArray);
+int min = MinSum(sumArray);
+Console.WriteLine($"Строкой с наименьшей суммой является: {min} строка");
+
---
+++ Practice_8/dz_task_58/Program.cs
@@ -0,0 +1,85 @@
+﻿/* Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+Console.Clear();
+
+int[,] GenerateArray(int colLength, int rowLength, int start, int finish)
+{
+    int [,] array = new int[colLength, rowLength];
+    for (int i = 0; i < colLength; i++)
+    {
+        for (int j = 0; j < rowLength; j++)
+        {
+            array[i,j] = new Random().Next(start, finish +1);
+        }
+    }
+    return array;
+}
+
+void PrintInColor(string data, ConsoleColor color)
+{
+     Console.ForegroundColor = color;
+     Console.Write(data);
+     Console.ResetColor();
+}
+
+void printHeadOfArray(int length)
+{
+    Console.Write(" \t");
+    for (int i = 0; i < length; i++)
+    {
+        PrintInColor(i + "\t", ConsoleColor.DarkGreen);
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(int [,] array)
+{
+    printHeadOfArray(array.GetLength(1));
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        PrintInColor(i+"\t", ConsoleColor.Cyan);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] Multiply(int [,] arr1, int [,] arr2)
+{
+    int[,] resultMatrix = new int[arr1.GetLength(0), arr2.GetLength(1)];
+    for (int i = 0; i < arr2.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr1.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < arr1.GetLength(1); k++)
+            {
+                sum += arr1[i,k] * arr2[k,j];
+            }
+            resultMatrix[i,j] = sum;
+        }
+    }
+    return resultMatrix;
+}
+
+
+Console.WriteLine("Матрица 1:");
+int [,] array1 = GenerateArray(5, 5, 0, 20);
+
+int [,] array2 = GenerateArray(5, 5, 0, 20);
+PrintArray(array1);
+Console.WriteLine();
+Console.WriteLine("Матрица 2:");
+PrintArray(array2);
+int [,] result = Multiply(array1, array2);
+Console.WriteLine("Результат умножения матриц:");
+PrintArray(result);
